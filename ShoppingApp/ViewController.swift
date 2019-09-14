412eb1b11fea5872407949255b90c6f928d1bb8a
VLAKMAR <aruna.lakmal2@pearson.com>
2019-09-14 21:58:24 +0530

    signing,signup

--- ShoppingApp/ViewController.swift
+++ ShoppingApp/ViewController.swift
@@ -7,9 +7,61 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBAction func btnSignIn(_ sender: Any) {
+    
+    let urlString = "https://reqres.in/api/login"
+    
+        Alamofire.request(urlString,
+                          method: .post,
+                          parameters: ["email":"eve.holt@reqres.in","password": "cityslicka"],
+                          encoding: JSONEncoding.default,
+                          headers: nil).responseJSON {
+                            response in
+                            switch response.result {
+                            case .success:
+                                print("------------------")
+                                print(response)
+                                
+                                break
+                            case .failure(let error):
+                                
+                                print(error)
+                            }
+        }
+    }
+    
+    @IBAction func btnSignUp(_ sender: UIButton) {
+        
+        let urlString = "https://reqres.in/api/register"
+        
+        Alamofire.request(urlString,
+                          method: .post,
+                          parameters: ["email":"eve.holt@reqres.in","password": "cityslicka"],
+                          encoding: JSONEncoding.default,
+                          headers: nil).responseJSON {
+                            response in
+                            switch response.result {
+                            case .success:
+                                print(response)
+                                
+                                break
+                            case .failure(let error):
+                                
+                                print(error)
+                            }
+        }
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
